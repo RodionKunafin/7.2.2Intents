bbba50b39cf093cd4344a5bb29427692b0e5bfbc
RodionKunafin <george.t.o.p@mail.ru>
2020-05-26 10:56:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a722intents/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.a722intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText phoneText;
+    EditText smsText;
+    Button btnCall;
+    Button btnSms;
+    EditText phoneNumberForSms;
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 11;
+    private static final int MY_PERMISSIONS_SEND_SMS = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        phoneText = findViewById(R.id.phoneText);
+        smsText = findViewById(R.id.smsText);
+        btnCall = findViewById(R.id.btnCall);
+        btnSms = findViewById(R.id.btnSms);
+        phoneNumberForSms = findViewById(R.id.phoneNumberForSms);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callByNumber();
+            }
+        });
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSms();
+            }
+        });
+    }
+    private void callByNumber() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else {
+            Intent dialIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneText.getText()));
+           startActivity(dialIntent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callByNumber();
+                } else {
+                    finish();
+                }
+            }
+        }
+    }
+    private void sendSms() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_SEND_SMS);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneText.getText()));
+            startActivity(intent);
+            SmsManager smgr = SmsManager.getDefault();
+            smgr.sendTextMessage(""+phoneNumberForSms.getText(), null,
+                    ""+smsText.getText(), null, null);
+        }
+    }
+
+    public void onRequestPermissionsResultToSms(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendSms();
+                } else {
+                    finish();
+                }
+            }
+        }
+    }
+}
